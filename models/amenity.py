4ea0fa5d0916d5e238389d5f540bd7fdef35ab80
Ryuichi Miyazaki <rmiyazaki11@ucsbalum.com>
2019-11-07 05:18:31 +0000

    First commit for model Amenity

---
+++ models/amenity.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+This is a Amenity Module for AirBnB
+"""
+
+class Amenity(BaseModel):
+    """Amenity Model"""
+
+    def __init__(self, *args, **kwargs):
+    """Initialize Amenity instance"""
+    super().__init__(self, *args, **kwargs)
+
+    name = ''
