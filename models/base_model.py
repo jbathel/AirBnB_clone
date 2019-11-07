e88b7587bcf9d01e881894e2cad8c236931ecb87
Jessica Bathel <811@holbertonschool.com>
2019-11-07 01:15:40 +0000

    Created base model class for tasks 3 and 4

---
+++ models/base_model.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python3
+"""
+This is a Base Module for AirBnB
+"""
+import datetime
+import json
+import uuid
+
+
+class BaseModel():
+    """Base Model"""
+
+    def __init__(self, *args, **kwargs):
+        """Initialize Public Attributes"""
+        if kwargs:
+            for key, value in kwargs.items():
+                if key == "created_at" or key == "updated_at":
+                    value = datetime.datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
+                self.key = value
+
+        try:
+            self.id
+        except:
+            self.id = uuid.uuid4()
+        try:
+            self.created_at
+        except:
+            self.created_at = datetime.datetime.now()
+
+    def __str__(self):
+        """..."""
+        return "[{}] ({}) {}".format(self.__class__.__name__,
+                                     self.id, self.__dict__)
+    def save(self):
+        """..."""
+        self.update_at = datetime.datetime.now()
+
+    def to_dict(self):
+        """..."""
+        dictionary = {}
+        dictionary["__class__"] = self.__class__.__name__
+        if self.__dict__:
+            for key, value in self.__dict__.items():
+                if isinstance(value, datetime.datetime) is True:
+                    value = value.isoformat()
+                dictionary[key] = value
+        return dictionary
